2787951b58f0ece22dfd772290a13074e8c6f6ff
jo-kwsm <ytk.kwsm@gmail.com>
2020-10-08 17:20:34 +0900

    地区を取得する際に県を選択してしまった場合はskip

--- downloader.py
+++ downloader.py
@@ -8,60 +8,62 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import chromedriver_binary
 
+#設定
+url = "https://www.data.jma.go.jp/gmd/risk/obsdl/index.php"
+save_root_dir = "./data"
+dl_dir = os.path.join(os.getcwd(),'dl/')
+dl_data_file = os.path.join(dl_dir,"data.csv")
+year_init = 2020
+year_range = 2
+wait_dl = 3
+wait_change = 0.5
+
+#各ボタンのxpath
+area_button = '//*[@id="stationButton"]'
+element_button = '//*[@id="elementButton"]'
+hour_button = '//*[@id="aggrgPeriod"]/div/div[1]/div[1]/label/input'
+period_button = '//*[@id="periodButton"]'
+dl_button = '//*[@id="csvdl"]/img'
+year_init_button = '//*[@id="chpr1y"]'
+year_from_button = '//*[@id="selectPeriod"]/div/div[1]/div[2]/div[2]/select[1]'
+year_to_button = '//*[@id="selectPeriod"]/div/div[1]/div[2]/div[3]/select[1]'
+city_name_path = '//*[@id="selectedStationList"]/div/div[1]'
+
+#県名のリスト
+#県のx_pathを辞書で管理
+prefectures = {
+    '東京都':'//*[@id="pr44"]',
+    '沖縄県':'//*[@id="prefectureTable"]/tbody/tr[16]/td',
+    '茨城県':'//*[@id="pr40"]',
+    '千葉県':'//*[@id="pr45"]',
+    '埼玉県':'//*[@id="pr43"]',
+    '栃木県':'//*[@id="pr41"]',
+    '福島県':'//*[@id="pr36"]',
+}
+#取得要素のリスト
+#テスト時は項目を絞る
+#TODO　本番環境は全項目のダウンロード
+#TODO　カラム名の指定の仕方
+elements = ["気温", "降水量", "日照時間", "風向・風速"]
+columns = {
+    "気温":["temperature","temperature:quality","temperature:homogeneity"],
+    "降水量":["precipitation","precipitation:quality","precipitation:homogeneity"],
+    "日照時間":["daylight","daylight:quality","daylight:homogeneity"],
+    "風向・風速":["wind_velocity","wind_velocity:quality","wind_direction","wind_direction:quality","wind:homogeneity"],
+}
+#elements = ["気温", "降水量","降雪の深さ","積雪の深さ","日照時間","風向・風速","全天日射量","現地気圧","海面気圧","相対湿度","蒸気圧","露点温度","天気","雲量","視程"]
+
+#TODO 本番環境はブラウザを開かない
+prefs = {
+    'download.default_directory' : dl_dir,
+    'download.prompt_for_download' : False,
+    'download.directory_upgrade' : True,
+}
+
 def main():
-    #設定
-    url = "https://www.data.jma.go.jp/gmd/risk/obsdl/index.php"
-    save_root_dir = "./data"
-    dl_dir = os.path.join(os.getcwd(),'dl/')
-    dl_data_file = os.path.join(dl_dir,"data.csv")
-    year_init = 2020
-    year_range = 2
-    wait_dl = 2
-
-    #各ボタンのxpath
-    area_button = '//*[@id="stationButton"]'
-    element_button = '//*[@id="elementButton"]'
-    hour_button = '//*[@id="aggrgPeriod"]/div/div[1]/div[1]/label/input'
-    period_button = '//*[@id="periodButton"]'
-    dl_button = '//*[@id="csvdl"]/img'
-    year_init_button = '//*[@id="chpr1y"]'
-    year_from_button = '//*[@id="selectPeriod"]/div/div[1]/div[2]/div[2]/select[1]'
-    year_to_button = '//*[@id="selectPeriod"]/div/div[1]/div[2]/div[3]/select[1]'
-    city_name_path = '//*[@id="selectedStationList"]/div/div[1]'
-
-    #県名のリスト
-    #県のx_pathを辞書で管理
-    prefectures = {
-        '東京都':'//*[@id="pr44"]',
-        '沖縄県':'//*[@id="prefectureTable"]/tbody/tr[16]/td',
-        '茨城県':'//*[@id="pr40"]',
-        '千葉県':'//*[@id="pr45"]',
-        '埼玉県':'//*[@id="pr43"]',
-        '栃木県':'//*[@id="pr41"]',
-        '福島県':'//*[@id="pr36"]',
-    }
-    #取得要素のリスト
-    #テスト時は項目を絞る
-    #TODO　本番環境は全項目のダウンロード
-    #TODO　カラム名の指定の仕方
-    elements = ["気温", "降水量", "日照時間", "風向・風速"]
-    columns = {
-        "気温":["temperature","temperature:quality","temperature:homogeneity"],
-        "降水量":["precipitation","precipitation:quality","precipitation:homogeneity"],
-        "日照時間":["daylight","daylight:quality","daylight:homogeneity"],
-        "風向・風速":["wind_velocity","wind_velocity:quality","wind_direction","wind_direction:quality","wind:homogeneity"],
-    }
-    #elements = ["気温", "降水量","降雪の深さ","積雪の深さ","日照時間","風向・風速","全天日射量","現地気圧","海面気圧","相対湿度","蒸気圧","露点温度","天気","雲量","視程"]
-
     #ブラウザ閲覧時のオプションを指定するオブジェクト"options"を作成
     options= Options()
     #必要に応じてオプションを追加
-    #TODO 本番環境はブラウザを開かない
-    prefs = {
-        'download.default_directory' : dl_dir,
-        'download.prompt_for_download' : False,
-        'download.directory_upgrade' : True,
-    }
     options.add_experimental_option("prefs",prefs)
 
     #TODO クラスでドライバーを管理
@@ -75,6 +77,7 @@ def main():
     os.mkdir(save_root_dir)
 
     for prefecture, prefecture_path in prefectures.items():
+        print(prefecture)
         #県名のディレクトリを作成
         save_dir = os.path.join(save_root_dir, prefecture+'/')
         os.mkdir(save_dir)
@@ -82,28 +85,28 @@ def main():
         driver = webdriver.Chrome(chrome_options=options)
         driver.get(url)
         #立ち上がりを待つ
-        time.sleep(0.5)
+        time.sleep(wait_change)
         #県の項目に移動
         driver.find_elements_by_xpath(prefecture_path)[0].click()
-        time.sleep(0.5)
+        time.sleep(wait_change)
         #地区のx_pathを取得
         cities = []
-        for to in range(1,1000,2):
+        for to in range(1000):
             path = '//*[@id="stationMap"]/div['+str(to)+']'
             try:
                 driver.find_element_by_xpath(path)
             except:
-                for id in range(1,to,2):
-                    cities.append('//*[@id="stationMap"]/div['+str(id)+']')
+                for id in range(to):
+                    cities.append('//*[@id="stationMap"]/div['+str(id)+']/div')
                 break
         #エラー項目を辞書で保存
         error_cities = {}
 
         for city in tqdm(cities):
-            #TODO 都道府県を選択した場合continue
-            #city_name = driver.find_element_by_xpath#(city_name_path).title
-            #if city_name[-1] in '都道府県':
-            #    continue
+            #都道府県を選択した場合continue
+            city_name = driver.find_element_by_xpath(city).get_attribute("title")
+            if city_name[-1] in '都道府県':
+                continue
             #地区選択
             driver.find_elements_by_xpath(city)[0].click()
             city_name = driver.find_element_by_xpath(city_name_path).text
@@ -170,7 +173,8 @@ def main():
             #地区選択解除
             driver.find_elements_by_xpath(city)[0].click()
         #保存できなかった地域を出力
-        print(','.join(error_cities))
+        for k,v in error_cities.items():
+            print(":".join([k,v]))
         #ドライバーを閉じる
         driver.quit()
     
